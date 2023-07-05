b69bd9bbe8eae5987c061378494f9a6e3c2bb7a4
Seoulless25 <k.j.jackson1016@gmail.com>
2023-07-04 23:06:38 -0400

    Update 6
    Added images

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const allAlbumsCovers = [
-    'https://imgbox.com/p27wzFJg',
-    'https://images2.imgbox.com/e6/5b/MY6T2p6a_o.jpg'
+    'https://images2.imgbox.com/7f/ee/p27wzFJg_o.jpg',
+    'https://images2.imgbox.com/e6/5b/MY6T2p6a_o.jpg',
+    'https://images2.imgbox.com/4c/c3/kKtDYoHn_o.jpg',
+    'https://images2.imgbox.com/b1/e7/VQpyBt00_o.jpg',
+    'https://images2.imgbox.com/fa/29/9L5kcvW8_o.jpg',
+    'https://images2.imgbox.com/5a/8a/4HiOgPDO_o.jpg',
+    'https://images2.imgbox.com/67/e0/DjD114yZ_o.jpg',
+    'https://images2.imgbox.com/54/3a/JadlihvE_o.jpg',
+    'https://images2.imgbox.com/59/df/Gari6T5l_o.jpg',
+    'https://images2.imgbox.com/4b/83/QP53Fh1n_o.jpg',
+    'https://images2.imgbox.com/b2/d6/x7PNCUYa_o.jpg',
+    'https://images2.imgbox.com/fe/09/LX5oLCAD_o.jpg',
  ]
 
 let randomIndex = Math.floor( Math.random() * allAlbumsCovers.length);
