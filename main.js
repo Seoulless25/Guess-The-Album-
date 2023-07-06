2538fa7747c6701b84c30a090433fe2e4148a71c
Seoulless25 <k.j.jackson1016@gmail.com>
2023-07-06 15:22:33 -0400

    Update 15 - Complete

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 const allAlbumsCovers = [
-    {AlbumImage: 'https://images2.imgbox.com/7f/ee/p27wzFJg_o.jpg', AlbumName: 'Kids See Ghosts'},
-    {AlbumImage: 'https://images2.imgbox.com/e6/5b/MY6T2p6a_o.jpg', AlbumName: 'To Pimp A Butterfly'}, 
-    {AlbumImage: 'https://images2.imgbox.com/4c/c3/kKtDYoHn_o.jpg', AlbumName: '808s & Heartbreak'},
-    {AlbumImage: 'https://images2.imgbox.com/b1/e7/VQpyBt00_o.jpg', AlbumName: 'Because The Internet'},
-    {AlbumImage: 'https://images2.imgbox.com/fa/29/9L5kcvW8_o.jpg', AlbumName: 'Black On Both Sides'},
-    {AlbumImage: 'https://images2.imgbox.com/5a/8a/4HiOgPDO_o.jpg', AlbumName: 'Circles'},
-    {AlbumImage: 'https://images2.imgbox.com/67/e0/DjD114yZ_o.jpg', AlbumName: 'College Dropout'},
-    {AlbumImage: 'https://images2.imgbox.com/54/3a/JadlihvE_o.jpg', AlbumName: 'Good Kid Maad City'},
-    {AlbumImage: 'https://images2.imgbox.com/59/df/Gari6T5l_o.jpg', AlbumName: 'The Forever Story'},
-    {AlbumImage: 'https://images2.imgbox.com/4b/83/QP53Fh1n_o.jpg', AlbumName: 'Stankonia'},
-    {AlbumImage: 'https://images2.imgbox.com/b2/d6/x7PNCUYa_o.jpg', AlbumName: 'Madvillainy'},
-    {AlbumImage: 'https://images2.imgbox.com/fe/09/LX5oLCAD_o.jpg', AlbumName: 'Melt My Eyez See Your Future'},
-    {AlbumImage: 'https://images2.imgbox.com/51/a3/YY986KBJ_o.jpg', AlbumName: 'Views'},
-    {AlbumImage: 'https://images2.imgbox.com/9d/0a/2nzPEdG8_o.jpg', AlbumName: 'The House Is Burning'},
-    {AlbumImage: 'https://images2.imgbox.com/02/f7/fz67c9Rc_o.jpg', AlbumName: 'Swimming'},
-    {AlbumImage: 'https://images2.imgbox.com/4c/b9/jvWChafz_o.jpg', AlbumName: 'Ramona Park Broke My Heart'},
-    {AlbumImage: 'https://images2.imgbox.com/f6/25/D3EX25Ju_o.jpg', AlbumName: 'Rodeo'},
-    {AlbumImage: 'https://images2.imgbox.com/4a/7c/VqjfZAIE_o.jpg', AlbumName: 'Pinata'},
-    {AlbumImage: 'https://images2.imgbox.com/34/09/dSbgnZJP_o.jpg', AlbumName: 'The Offseason'},
-    {AlbumImage: 'https://images2.imgbox.com/b4/d6/Sc3ZFoza_o.jpg', AlbumName: 'Man On The Moon'},
-    {AlbumImage: 'https://images2.imgbox.com/50/2a/brlgEcCI_o.jpg', AlbumName: 'Luv Is Rage 2'},
-    {AlbumImage: 'https://images2.imgbox.com/19/e4/cMB9crMU_o.jpg', AlbumName: 'Legends Never Die'},
-    {AlbumImage: 'https://images2.imgbox.com/cf/ee/4AvHFFyu_o.jpg', AlbumName: 'Heroes & Villains'},
-    {AlbumImage: 'https://images2.imgbox.com/10/33/dhuIuQSI_o.jpg', AlbumName: 'Flower Boy'},
-    {AlbumImage: 'https://images2.imgbox.com/35/55/5w4A9hEf_o.jpg', AlbumName: 'Drill Music In Zion'},
-    {AlbumImage: 'https://images2.imgbox.com/ba/3f/9eFFHGK7_o.jpg', AlbumName: 'Coloring Book'},
-    {AlbumImage: 'https://images2.imgbox.com/3b/bd/AX4QK8vH_o.jpg', AlbumName: 'Business Is Business'}
+    {AlbumImage: 'https://images2.imgbox.com/7f/ee/p27wzFJg_o.jpg', AlbumName: 'Kids See Ghosts', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/e6/5b/MY6T2p6a_o.jpg', AlbumName: 'To Pimp A Butterfly', AlbumGenre: 'Hip Hop'}, 
+    {AlbumImage: 'https://images2.imgbox.com/4c/c3/kKtDYoHn_o.jpg', AlbumName: '808s & Heartbreak', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/b1/e7/VQpyBt00_o.jpg', AlbumName: 'Because The Internet', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/fa/29/9L5kcvW8_o.jpg', AlbumName: 'Black On Both Sides', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/5a/8a/4HiOgPDO_o.jpg', AlbumName: 'Circles', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/67/e0/DjD114yZ_o.jpg', AlbumName: 'College Dropout', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/54/3a/JadlihvE_o.jpg', AlbumName: 'Good Kid Maad City', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/59/df/Gari6T5l_o.jpg', AlbumName: 'The Forever Story', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/4b/83/QP53Fh1n_o.jpg', AlbumName: 'Stankonia', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/b2/d6/x7PNCUYa_o.jpg', AlbumName: 'Madvillainy', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/fe/09/LX5oLCAD_o.jpg', AlbumName: 'Melt My Eyez See Your Future', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/51/a3/YY986KBJ_o.jpg', AlbumName: 'Views', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/9d/0a/2nzPEdG8_o.jpg', AlbumName: 'The House Is Burning', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/02/f7/fz67c9Rc_o.jpg', AlbumName: 'Swimming', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/4c/b9/jvWChafz_o.jpg', AlbumName: 'Ramona Park Broke My Heart', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/f6/25/D3EX25Ju_o.jpg', AlbumName: 'Rodeo', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/4a/7c/VqjfZAIE_o.jpg', AlbumName: 'Pinata', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/34/09/dSbgnZJP_o.jpg', AlbumName: 'The Offseason', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/b4/d6/Sc3ZFoza_o.jpg', AlbumName: 'Man On The Moon', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/50/2a/brlgEcCI_o.jpg', AlbumName: 'Luv Is Rage 2', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/19/e4/cMB9crMU_o.jpg', AlbumName: 'Legends Never Die', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/cf/ee/4AvHFFyu_o.jpg', AlbumName: 'Heroes & Villains', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/10/33/dhuIuQSI_o.jpg', AlbumName: 'Flower Boy', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/35/55/5w4A9hEf_o.jpg', AlbumName: 'Drill Music In Zion', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/ba/3f/9eFFHGK7_o.jpg', AlbumName: 'Coloring Book', AlbumGenre: 'Hip Hop'},
+    {AlbumImage: 'https://images2.imgbox.com/3b/bd/AX4QK8vH_o.jpg', AlbumName: 'Business Is Business', AlbumGenre: 'Hip Hop'}
  ]
-
 
 let randomIndex = Math.floor( Math.random() * allAlbumsCovers.length);
 let randomCover = allAlbumsCovers[randomIndex].AlbumImage;
@@ -56,6 +55,7 @@ function correct() {
     let correctMessage = document.getElementById('correctMessage')
     correctMessage.textContent = "CORRECT!"
     score++;
+    oldScore = score;
     document.getElementById('score').textContent = "Score: " + score;
     setTimeout(() => correctMessage.textContent = '', 3000 )
     initNewPage();
@@ -64,7 +64,6 @@ function correct() {
 function incorrect() {
     let incorrectMessage = document.getElementById('incorrectMessage')
     incorrectMessage.textContent = "TRY AGAIN!"
-    oldScore = score;
     score = 0;
     setTimeout(() => incorrectMessage.textContent = '', 3000 )
 }
