b12f6a6ecfd827b637c7dc5ca1cb741cdb000b14
Seoulless25 <k.j.jackson1016@gmail.com>
2023-07-06 13:11:05 -0400

    Update 14 - Basically Finished

--- main.js
+++ main.js
@@ -10,13 +10,30 @@ const allAlbumsCovers = [
     {AlbumImage: 'https://images2.imgbox.com/59/df/Gari6T5l_o.jpg', AlbumName: 'The Forever Story'},
     {AlbumImage: 'https://images2.imgbox.com/4b/83/QP53Fh1n_o.jpg', AlbumName: 'Stankonia'},
     {AlbumImage: 'https://images2.imgbox.com/b2/d6/x7PNCUYa_o.jpg', AlbumName: 'Madvillainy'},
-    {AlbumImage: 'https://images2.imgbox.com/fe/09/LX5oLCAD_o.jpg', AlbumName: 'Melt My Eyez See Your Future'}
+    {AlbumImage: 'https://images2.imgbox.com/fe/09/LX5oLCAD_o.jpg', AlbumName: 'Melt My Eyez See Your Future'},
+    {AlbumImage: 'https://images2.imgbox.com/51/a3/YY986KBJ_o.jpg', AlbumName: 'Views'},
+    {AlbumImage: 'https://images2.imgbox.com/9d/0a/2nzPEdG8_o.jpg', AlbumName: 'The House Is Burning'},
+    {AlbumImage: 'https://images2.imgbox.com/02/f7/fz67c9Rc_o.jpg', AlbumName: 'Swimming'},
+    {AlbumImage: 'https://images2.imgbox.com/4c/b9/jvWChafz_o.jpg', AlbumName: 'Ramona Park Broke My Heart'},
+    {AlbumImage: 'https://images2.imgbox.com/f6/25/D3EX25Ju_o.jpg', AlbumName: 'Rodeo'},
+    {AlbumImage: 'https://images2.imgbox.com/4a/7c/VqjfZAIE_o.jpg', AlbumName: 'Pinata'},
+    {AlbumImage: 'https://images2.imgbox.com/34/09/dSbgnZJP_o.jpg', AlbumName: 'The Offseason'},
+    {AlbumImage: 'https://images2.imgbox.com/b4/d6/Sc3ZFoza_o.jpg', AlbumName: 'Man On The Moon'},
+    {AlbumImage: 'https://images2.imgbox.com/50/2a/brlgEcCI_o.jpg', AlbumName: 'Luv Is Rage 2'},
+    {AlbumImage: 'https://images2.imgbox.com/19/e4/cMB9crMU_o.jpg', AlbumName: 'Legends Never Die'},
+    {AlbumImage: 'https://images2.imgbox.com/cf/ee/4AvHFFyu_o.jpg', AlbumName: 'Heroes & Villains'},
+    {AlbumImage: 'https://images2.imgbox.com/10/33/dhuIuQSI_o.jpg', AlbumName: 'Flower Boy'},
+    {AlbumImage: 'https://images2.imgbox.com/35/55/5w4A9hEf_o.jpg', AlbumName: 'Drill Music In Zion'},
+    {AlbumImage: 'https://images2.imgbox.com/ba/3f/9eFFHGK7_o.jpg', AlbumName: 'Coloring Book'},
+    {AlbumImage: 'https://images2.imgbox.com/3b/bd/AX4QK8vH_o.jpg', AlbumName: 'Business Is Business'}
  ]
 
 
 let randomIndex = Math.floor( Math.random() * allAlbumsCovers.length);
 let randomCover = allAlbumsCovers[randomIndex].AlbumImage;
 let imagePlaceHolder = document.getElementById('imagePlaceHolder');
+let score = 0;
+let oldScore = 0;
 
 let coverAlbum = document.createElement('img');
 coverAlbum.src = randomCover;
@@ -38,14 +55,18 @@ function checkAnswer() {
 function correct() {
     let correctMessage = document.getElementById('correctMessage')
     correctMessage.textContent = "CORRECT!"
-    setTimeout(() => correctMessage.textContent = '', 5000 )
+    score++;
+    document.getElementById('score').textContent = "Score: " + score;
+    setTimeout(() => correctMessage.textContent = '', 3000 )
     initNewPage();
 }
 
 function incorrect() {
     let incorrectMessage = document.getElementById('incorrectMessage')
     incorrectMessage.textContent = "TRY AGAIN!"
-    setTimeout(() => incorrectMessage.textContent = '', 5000 )
+    oldScore = score;
+    score = 0;
+    setTimeout(() => incorrectMessage.textContent = '', 3000 )
 }
 
 function initNewPage() {
@@ -61,3 +82,4 @@ function initNewPage() {
     coverAlbum.src = randomCover;
     imagePlaceHolder.appendChild(coverAlbum);
 }
+
